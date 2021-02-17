b2d743ed52cf60bf9c39942fba480613bf30cde0
Pinky Mononyane <64901994+Pinky-alt-spec@users.noreply.github.com>
2021-02-17 04:52:24 -0800

    Update settings.py
    fix templates

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'Portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR),"templates"],
+        'DIRS': [r'C:\Users\PINKY\PycharmProjects\Portfolio\Myportfolio\templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
