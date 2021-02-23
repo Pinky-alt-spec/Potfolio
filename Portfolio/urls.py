1c13d561a07bbc07bfd39fb996b55ff0e1b992be
Pinky-alt-spec <pinkymononyane@gmail.com>
2021-02-23 06:21:41 -0800

    add templates

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
 
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    url((r'', include('templates.urls')),)
 ]
 
