63381da134d4548359d52a4f75dd55da201b2e35
Pinky-alt-spec <pinkymononyane@gmail.com>
2021-02-23 07:46:44 -0800

    add templates

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -70,7 +70,8 @@ ROOT_URLCONF = 'Portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'Myportfolio','templates', 'Myportfolio')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
