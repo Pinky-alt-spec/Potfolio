073b0c20414c7513f8c9628d7630082dd8e26327
Pinky-alt-spec <pinkymononyane@gmail.com>
2021-02-26 09:49:57 -0800

    Add captcha

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -39,7 +39,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'pinkymononyane@gmail.com'
-EMAIL_HOST_PASSWORD = 'pinky@1Mononyane'
+EMAIL_HOST_PASSWORD = 'Mma!Tshegofatso@Pinky#Lebo$'
 
 
 # Application definition
