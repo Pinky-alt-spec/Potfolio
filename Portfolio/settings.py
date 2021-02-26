3b7df4eee54ed49fc7df37d03fda427871a70fce
Pinky Mononyane <64901994+Pinky-alt-spec@users.noreply.github.com>
2021-02-26 03:53:13 -0800

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -136,6 +136,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+if os.getcwd() == '/app':
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    DEBUG = False
+
 
 
 
