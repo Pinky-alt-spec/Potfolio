3fa505ad42b8543761a81aabc2ff0160822cef5b
Pinky-alt-spec <pinkymononyane@gmail.com>
2021-02-26 22:41:23 -0800

    static

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.views.static import serve
 from django.conf.urls import url
@@ -29,5 +30,6 @@ urlpatterns = [
     url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+urlpatterns += staticfiles_urlpatterns()
 
 
