95633ca201253f51217635bf2db70dec00ccbbaa
Pinky-alt-spec <pinkymononyane@gmail.com>
2021-02-26 20:50:47 -0800

    add media

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -135,11 +135,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 
-# if os.getcwd() == '/app':
-#     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-#     SECURE_SSL_REDIRECT = True
-#     DEBUG = False
 
 
 
