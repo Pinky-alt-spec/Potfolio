5fd1edfccd4f5af558af3838e51f4d91714a9395
Pinky Mononyane <64901994+Pinky-alt-spec@users.noreply.github.com>
2021-06-05 18:43:44 +0200

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -45,7 +45,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'pinkymononyane@gmail.com'
-EMAIL_HOST_PASSWORD = 'siyxvoekehgpgysf'
+EMAIL_HOST_PASSWORD = 'brhdpctzwlpvljju'
 
 
 # Application definition
